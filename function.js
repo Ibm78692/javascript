f083bddb5bf648058510ba1b7116793baacb9108
Ibm78692 <mdibrahimansari79.mia@gmail.com>
2024-06-05 11:16:12 +0000

    function

---
+++ function.js
@@ -0,0 +1,9 @@
+function addTwoNumbers(num1, num2){
+    console.log(num1+num2);
+    console.log(num1-num2);
+    console.log(num1*num2);
+    console.log(num1/num2);
+    console.log(num1%num2);
+    console.log(num1**num2);
+}
+addTwoNumbers(10, 5)
